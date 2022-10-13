01bafb835eb81d75b5e0ae0be9cf14afe41ebaca
Varun Kumar Bansal <varunbansal326@gmail.com>
2022-10-13 16:12:02 +0530

    Homepage added

---
+++ Frontend/src/components/Footer.js
@@ -0,0 +1,10 @@
+import '../css/footer.css'
+
+export default function Footer() {
+    return (
+        <footer>
+            <a href="/aboutus" class = "content" >About Us</a>
+            <p class = "content" >© Group-27</p>
+        </footer>
+    )
+}
