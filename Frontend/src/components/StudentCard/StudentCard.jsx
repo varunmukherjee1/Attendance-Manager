cbd884ebcdc6c4185aab710874803e6cd0dd81d9
Varun <varunmukherjee7@gmail.com>
2022-11-13 21:01:29 +0530

    fetching student classes properly

--- Frontend/src/components/StudentCard/StudentCard.jsx
+++ Frontend/src/components/StudentCard/StudentCard.jsx
@@ -1,14 +1,19 @@
 import React from 'react'
 import classes from "./StudentCard.module.css"
+import profile_pic from "../../assets/profile_pic.svg"
 // import QrModal from '../QrModal/QrModal'
 // import {QRious} from 'react-qrious'
 
 function StudentCard(props) {
+
+  // console.log("props :-")
+  // console.log(props);
+
   return (
     <div className={classes.card_container}>
       <div className = {classes.card}>
           <p className={classes.class_name}>{props.title}</p>
-          <img src={props.src} alt={"Image not found"} className={classes.teacher_img}/>
+          <img src={profile_pic} alt={"Image not found"} className={classes.teacher_img}/>
           <p className={classes.teacher_name}>{props.teacher}</p>
           <input type={"button"} value={"Get QR Code"}  onClick={props.qrCode}/><br />
           <input type={"button"} value={"See Attendance"} onClick={props.seeAtt}/>
--- Frontend/src/pages/StudentDashboard/StudentDashboard.jsx
+++ Frontend/src/pages/StudentDashboard/StudentDashboard.jsx
@@ -1,3 +1,9 @@
+import { useEffect, useState } from 'react';
+import axios from "axios"
+import {useSelector , useDispatch } from 'react-redux';
+import {loadingActions} from "../../store/loadingSlice"
+import image from "../../assets/profile_pic.svg"
+
 import Dashboard from '../../components/Dashboard/Dashboard'
 import Card from "../../components/StudentCard/StudentCard";
 import student from "./StudentDashboard.module.css";
@@ -6,6 +12,8 @@ import {studentsClasses} from "../../constants/StudentClasses"
 
 export default function StudentDashboard() {
 
+    const [stdClasses,setStdClasses] = useState([]);
+
     const getQrCode = () => {
         console.log("QR Code button clicked");
     }
@@ -13,13 +21,48 @@ export default function StudentDashboard() {
     const seeAtt = () => {
         console.log("temp");
     }
+
+    const user = useSelector(state => state.user.user)
+    const dispatch = useDispatch();
+
+    const getClasses = async () => {
+        try {
+            dispatch(loadingActions.showLoading())
+            const res = await axios.get("/api/getClasses")
+            dispatch(loadingActions.hideLoading());
+
+            // console.log(res.data);
+            const temp = res.data.data.filter((cls) => {
+                let ret = false;
+                cls.students.forEach((std) => {
+                    // console.log(`std = ${std.roll_number}`);
+                    if(std.roll_number === user.roll_number){
+                        ret = true;
+                    }
+                })
+                return ret;
+            })
+
+            setStdClasses(temp)
+        } catch (error) {
+            dispatch(loadingActions.hideLoading())
+            console.log("Error in fetching Classes:");
+            console.log(error)
+        }
+    }
+
+    useEffect(() => {
+        getClasses();
+    },[]);
+
+    // console.log(stdClasses);
     
     return (
         <Dashboard>
             <div className={student.classes}>
-                {studentsClasses.map(c => {
+                {stdClasses.map(c => {
                     return (
-                        <Card key={c.title} src={c.src} title={c.title} teacher={c.teacher}  qrCode={getQrCode} seeAtt={seeAtt}/>
+                        <Card key={c._id} src={image} title={c.name} teacher={c.teachers[0].email}  qrCode={getQrCode} seeAtt={seeAtt}/>
                     )
                 })}
             </div>
